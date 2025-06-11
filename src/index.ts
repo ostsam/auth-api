506fd4ab0e45af93066fa1f290cf2c0e49586d40
ostsam <ost.sam@gmail.com>
2025-06-11 10:50:49 -0400

    initialized Elysia

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+new Elysia()
+  .get("/", () => "Hello Elysia")
+  .listen(3000)
+  .get("/api/public", () => "This is public information.")
+  .get("/api/protected", () => "Only admin should be able to see this.");
