8e5fc5caaf70e8e529c550cb13a19878b7d22ce7
ostsam <ost.sam@gmail.com>
2025-06-11 16:55:45 -0400

    authentication with secrets

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,50 @@
-import { Elysia } from "elysia";
-import { swagger } from "@elysiajs/swagger";
+import swagger from "@elysiajs/swagger";
+import { Elysia, status } from "elysia";
 
-new Elysia()
-  .use(swagger())
-  .get("/", () => "Hello Elysia")
-  .listen(3000)
-  .get("/api/public", () => "This is public information.")
-  .get("/api/protected", () => "Only admin should be able to see this.");
+const PORT = 3000;
+
+const validApiKeys = new Set(["api-key-123", "api-key-456"]);
+
+const protectedRoutes = new Elysia()
+  .derive({ as: "local" }, (request) => {
+    return {
+      user: { name: "sam", role: "admin" },
+      isAuthenticated: true,
+    };
+  })
+  .onBeforeHandle({ as: "local" }, (request) => {
+    const headers = request.headers;
+    const apiKey = headers["x-api-key"];
+
+    if (!apiKey) return status(401);
+
+    const isAuthenticated = validApiKeys.has(apiKey);
+
+    if (!isAuthenticated) return status(401);
+
+    if (request.user.role !== "admin") return status(401);
+  })
+  .get("/protected", () => {
+    return {
+      message: "[hacker voice] you're in. now try /protected-with-context",
+    };
+  })
+  .get("/protected-with-context", ({ user, isAuthenticated }) => {
+    return {
+      message: "Enhance the pixels.",
+      user: user,
+      authenticated: isAuthenticated,
+    };
+  });
+
+const app = new Elysia()
+  .use(swagger({ path: "/api-docs" }))
+  .get("/", () => {
+    return { message: "this is a public directory" };
+  })
+  .use(protectedRoutes)
+  .listen(PORT);
+
+console.log(
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+);
