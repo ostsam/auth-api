f5197c1185edc62c0f568b6ebbea83631bc545be
ostsam <ost.sam@gmail.com>
2025-06-12 15:20:10 -0400

    authentication

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,68 @@ import swagger from "@elysiajs/swagger";
 import { Elysia, status, t } from "elysia";
 import { users } from "./users";
 import { protectedRoutes } from "./protectedroutes";
-
+import { cookie } from "@elysiajs/cookie";
+import jwt from "@elysiajs/jwt";
 const PORT = 3000;
+
+if (process.env.JWT_TOKEN === undefined) {
+  throw new Error("JWT secret is missing!");
+}
 
 const app = new Elysia()
   .use(swagger({ path: "/api-docs" }))
+  .use(cookie())
   .get("/", () => {
     return { message: "this is a public directory" };
   })
   .use(protectedRoutes)
-  .post(
-    "/api/login",
+  .group("/api", (app) => {
+    app
+      .use(
+        jwt({
+          name: "jwt",
+          secret: process.env.JWT_TOKEN!,
+        })
+      )
+      .post(
+        "/login",
+        async ({ jwt, body, cookie: { auth } }) => {
+          const verifiedUser = users.filter(
+            (u) => u.username == body.username && u.password == body.password
+          );
+          if (verifiedUser.length == 0) return status(401);
+          const role = verifiedUser[0].role;
+          const id = verifiedUser[0].id;
+          const signedJWT = await jwt.sign({ id, role, exp: "10m" });
+          console.log(signedJWT);
 
-    async ({ body: { username, password }, jwt }) => {
-      const verifiedUser = users.filter(
-        (u) => u.username == username && u.password == password
-      );
-      if (!verifiedUser.length) return status(401);
-      const role = verifiedUser[0].role;
-      const id = verifiedUser[0].id;
-      const signedJWT = await jwt.sign({ id, role, exp: "1m" });
-      console.log(signedJWT);
-    },
-    {
-      body: t.Object({
-        username: t.String(),
-        password: t.String(),
-      }),
-    }
-  )
+          auth.set({
+            value: signedJWT,
+            httpOnly: true,
+            maxAge: 7 * 86400,
+            path: "/",
+          });
+
+          return "You are signed in!";
+        },
+        {
+          body: t.Object({
+            username: t.String(),
+            password: t.String(),
+          }),
+        }
+      )
+      .get("/chat", async (requestData) => {
+        const jwt = requestData.jwt;
+        const cookie = requestData.cookie;
+        const auth = cookie.auth;
+        const authValue = await jwt.verify(auth.value);
+        if (!authValue) return status(401);
+        return authValue;
+      });
+    return app;
+  })
+
   .listen(PORT);
 
 console.log(
--- src/protectedroutes.ts
+++ src/protectedroutes.ts
@@ -1,6 +1,6 @@
 import { Elysia, status } from "elysia";
-import jwt from "@elysiajs/jwt";
 import { users } from "./users";
+
 
 export const protectedRoutes = new Elysia()
   .derive(({ headers }) => {
@@ -9,26 +9,6 @@ export const protectedRoutes = new Elysia()
       bearer: auth?.startsWith("Bearer") ? auth.slice(7) : null,
     };
   })
-  .use(
-    jwt({
-      name: "jwt",
-      secret:
-        "739dddaf3e4c19f75921fa6823563402165a50a1a9de9784a8056766b50a2291",
-    })
-  )
-  .get("sign/:name", async ({ jwt, params: { name }, cookie: { auth } }) => {
-    const value = await jwt.sign({ name });
-
-    auth.set({
-      value,
-      httpOnly: true,
-      maxAge: 7 * 86400,
-      path: "/api/login",
-    });
-
-    return `Sign in as ${value}`;
-  })
-
   .get(
     "/api/protected",
     (bearer) => {
